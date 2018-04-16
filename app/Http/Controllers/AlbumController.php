eba1c8816639a6cebf9d9fa5017289adba426095
Luiz <argentinaluiz@gmail.com>
2018-04-16 13:54:20 -0300

    Primeiro commit

---
+++ app/Http/Controllers/AlbumController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Album;
+use Illuminate\Http\Request;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     */
+    public function index()
+    {
+        return Album::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Album::create(['name' => $request->name]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Album  $album
+     */
+    public function show(Album $album)
+    {
+        return $album;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Album $album)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Album $album)
+    {
+        //
+    }
+}
